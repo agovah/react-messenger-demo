c6f975daf252387b262c104315d710db3c028203
Nicolas S <sir.ago@gmail.com>
2020-08-08 17:34:32 +0200

    Updates

---
+++ precache-manifest.419eb277f578634df78dc6b05a928918.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a237df333bef231448d4568cd6985991",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d6308fee27552e77232",
+    "url": "/static/css/main.d4f833c6.chunk.css"
+  },
+  {
+    "revision": "18ffd6321b3865289279",
+    "url": "/static/js/2.28d4acca.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.28d4acca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d6308fee27552e77232",
+    "url": "/static/js/main.f1cd2778.chunk.js"
+  },
+  {
+    "revision": "464a0221b5d95a9b7ee7",
+    "url": "/static/js/runtime-main.8c8db920.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
